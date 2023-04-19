884619a219562eb1bf869f1ad9b3225b5e658893
wowba <leeyuwk54@gmail.com>
2023-04-19 22:53:13 +0900

    카운터 리듀서 생성 및 적용

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App value={0} onIncrement={function (): void {
+    throw new Error('Function not implemented.');
+  } } onDecrement={function (): void {
+    throw new Error('Function not implemented.');
+  } } />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import counter from './reducer';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
+
+const store = createStore(counter);
+
+const render = () => root.render(
   <React.StrictMode>
-    <App />
+    <App 
+      value={store.getState()}
+      // Action을 dispatch 하여 Redux Store에서 특정 Action을 인식한다.
+      onIncrement={() => store.dispatch({type: "INCREMENT"})}
+      onDecrement={() => store.dispatch({type: "DECREMENT"})}
+    />
   </React.StrictMode>
 );
+
+render()
+// 리듀서를 확인하기 위해 subscribe 해줘야한다. 아니면 컴포넌트가 변경되지 않는다.
+store.subscribe(render)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
