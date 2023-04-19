b1aaa9431de6b25bcefec8a046aab450bcce9afb
wowba <leeyuwk54@gmail.com>
2023-04-20 00:47:09 +0900

    미들웨어 작성

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './reducer';
 import { Provider } from 'react-redux';
 
@@ -11,7 +11,15 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const store = createStore(rootReducer);
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+  console.log("store", store)
+  console.log("action", action)
+  next(action) // 다음 동작으로 이동
+}
+
+const middleware = applyMiddleware(loggerMiddleware); 
+
+const store = createStore(rootReducer, middleware);
 
 const render = () => root.render(
   <React.StrictMode>
